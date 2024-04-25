f08423cb33837f6ee7487c57d736647d09a08824
fyiernzy <fyiernzy@gmail.com>
2024-04-25 10:30:39 +0800

    Add CommunityPage

---
+++ unijobs-zy/src/CommunityPage.js
@@ -0,0 +1,30 @@
+import NavBar from "./components/navbar/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProfileCard from "./components/community/ProfileCard/ProfileCard";
+import ExploreList from "./components/community/ExploreList/ExploreList";
+import { exploreListData, newsFeedList } from "./components/community/data";
+import NewsFeed from "./components/community/NewsFeed/NewsFeed";
+import SearchBar from "./components/community/SearchBar/SearchBar";
+
+function CommunityPage() {
+    return (
+      <>
+        
+        <NavBar />
+        <SearchBar />
+        <div className="main-content">
+          <div className="side-nav">
+            <ProfileCard
+              name="Mehrab Bozorgi"
+              title="Bachelor of Software Engineering"
+            />
+            <ExploreList exploreListData={exploreListData} />
+          </div>
+  
+          <NewsFeed newsFeedList={newsFeedList} />
+        </div>
+      </>
+    );
+  }
+  
+  export default CommunityPage;
