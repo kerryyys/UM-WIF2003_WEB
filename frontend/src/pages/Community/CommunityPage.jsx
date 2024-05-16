be348aa3b57efe4d0a0ea0a8c2f1e5a93b32a747
fyiernzy <fyiernzy@gmail.com>
2024-05-16 17:44:46 +0800

    Refactor CommunityPage using tailwindcss

--- frontend/src/pages/Community/CommunityPage.jsx
+++ frontend/src/pages/Community/CommunityPage.jsx
@@ -12,9 +12,9 @@ function CommunityPage() {
       {/* SearchBar */}
       <SearchBar />
 
-      <div className="main-content">
+      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center mt-5 sm:tw-flex-row sm:tw-justify-center sm:tw-items-start ">
         {/* Side Navigation Bar */}
-        <div className="side-nav">
+        <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-w-2/5 tw-gap-y-5">
           <ProfileCard
             name="Mehrab Bozorgi"
             title="Bachelor of Software Engineering"
