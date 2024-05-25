1ab0ae1f40667b8ad13938eb60afc0c5ee7eeb22
fyiernzy <fyiernzy@gmail.com>
2024-05-25 21:23:16 +0800

    Add postSchema and commentSchema

---
+++ backend/models/post.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose; // Destructure Schema from mongoose
+
+const postSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    image: { type: String },
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
