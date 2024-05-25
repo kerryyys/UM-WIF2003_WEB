8cea0feea96b26597f0335b7c7e148f99b58a997
fyiernzy <fyiernzy@gmail.com>
2024-05-25 21:40:40 +0800

    Change mongoDBUrl from localhost to IPV4 add, add new status

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,11 @@
 //i use this format because it is more organised
 export const PORT = 5050;
 
-export const mongoDBURL =
-  'mongodb://localhost:27017/unijobs';
+export const mongoDBURL = "mongodb://127.0.0.1:27017/unijobs";
 
-export const BASE_URL = 'http://localhost:5050';
+export const BASE_URL = "http://localhost:5050";
+
+export const Status = {
+  SUCCESS: 201,
+  ERROR: 500,
+};
