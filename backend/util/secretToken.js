2eb49124ba4360bf6e45680a8e5f73a6c515702a
howardhhao <wongwenhao19@gmail.com>
2024-05-31 13:02:51 +0800

    fix es module and export const

--- backend/util/secretToken.js
+++ backend/util/secretToken.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 
-module.exports.createSecretToken = (id) => {
+export const createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
