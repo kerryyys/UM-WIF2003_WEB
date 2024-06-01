ca0e18c3b331338966ba21929530c85cf2969c9b
fyiernzy <fyiernzy@gmail.com>
2024-06-01 18:50:56 +0800

    Rewrite the styling for ProfileCard

--- frontend/src/components/community/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/community/ProfileCard/ProfileCard.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import "../../../components-css/Community/ProfileCard.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import IconLabelPair from "./IconLabelPair";
 
 function ProfileCard({ name, title }) {
-  const bookmarkIcon = <i className="bi bi-bookmark profile-card-icon"></i>;
-  const bookmarkFilledIcon = (
-    <i className="bi bi-bookmark-fill profile-card-icon"></i>
-  );
+  const bookmarks = [
+    { label: "Saves", icon: "bi bi-bookmark-fill", link: "/" },
+    { label: "Jobs", icon: "bi bi-bookmark", link: "/" },
+  ];
 
   return (
-    <div className="profile-card">
-      <img
-        src="https://www.w3schools.com/howto/img_avatar.png"
-        alt="profile"
-        className="img-fluid rounded-circle mx-auto d-block profile-img"
-      ></img>
-      <p className="profile-name">{name}</p>
-      <p className="profile-title">{title}</p>
-      <hr className="profile-card-line" />
-      <div className="icon-label-pairs">
-        <IconLabelPair icon={bookmarkFilledIcon} label="My Saved List" />
-        <IconLabelPair icon={bookmarkIcon} label="My Jobs" />
+    <div className="tw-w-4/5 tw-p-3 tw-border tw-border-gray-300 tw-rounded-lg tw-shadow-md tw-bg-white">
+      <div className="tw-bg-gradient-to-tr tw-from-primary tw-to-cyan-600 tw-rounded-lg tw-flex tw-justify-center tw-items-center tw-py-5">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="profile"
+          className="tw-w-24 tw-h-24 tw-rounded-full tw-border-4 tw-border-white"
+        />
+      </div>
+      <div className="tw-px-5 tw-text-center">
+        <p className="tw-text-xl tw-font-bold tw-mt-2">{name}</p>
+        <p className="tw-text-gray-400 tw-mt-1">{title}</p>
+      </div>
+      <hr className="tw-w-11/12 tw-border-gray-300 tw-my-2" />
+      <div className="tw-flex tw-justify-center tw-items-center tw-gap-7">
+        {bookmarks.map((bookmark, index) => (
+          <IconLabelPair
+            key={index}
+            icon={bookmark.icon}
+            label={bookmark.label}
+          />
+        ))}
       </div>
     </div>
   );
