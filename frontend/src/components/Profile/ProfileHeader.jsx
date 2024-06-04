9c80a62e1d9db04ba02bd901f81f31cd0cc4cee3
user <22004795@siswa.um.edu.my>
2024-06-04 23:41:57 +0800

    handle null data of profile

--- frontend/src/components/Profile/ProfileHeader.jsx
+++ frontend/src/components/Profile/ProfileHeader.jsx
@@ -10,6 +10,18 @@ function ProfileHeader(props) {
   const { name, university, location, avatarSrc, headline, tags = [], userId } = props;
   const { user } = useUserContext();
 
+  const renderValue = (value, fieldName) => {
+    if (value !== undefined && value !== null && value !== ''&& value!=='undefined, undefined') {
+      return value;
+    } else {
+      return (
+        <span style={{ fontStyle: 'italic', color: '#858585' }}>
+          {`${fieldName} hasn't been set`}
+        </span>
+      );
+    }
+  };
+
   return (
     <Container style={{ margin: 'auto', width: '1000px' }}>
       <Row className="justify-content-center">
@@ -21,31 +33,49 @@ function ProfileHeader(props) {
           />
         </Col>
         <Col xs={12} md={5}>
-          <p style={{ marginTop: '10px', fontSize: '25px', fontWeight: 'bold' }}>{name}</p>
+          <p style={{ marginTop: '10px', fontSize: '25px', fontWeight: 'bold' }}>
+            {renderValue(name, 'Name')}
+          </p>
           <div style={{ display: 'flex', marginTop: '5px' }}>
-            <p style={{ color: '#858585', marginRight: '50px' }}>{location}</p>
-            <p style={{ color: '#2D4777' }}>{university}</p>
+            <p style={{ color: '#858585', marginRight: '50px' }}>
+              {renderValue(location, 'Location')}
+            </p>
+            <p style={{ color: '#2D4777' }}>
+              {renderValue(university, 'University')}
+            </p>
           </div>
-          <p style={{ marginTop: '5px' }}>{headline}</p>
+          <p style={{ marginTop: '5px' }}>
+            {renderValue(headline, 'Headline')}
+          </p>
           <div style={{ display: 'flex' }}>
-            {tags.map((tag, index) => (
-              <div
-                key={index}
-                style={{
-                  backgroundColor: '#80D1D5',
-                  color: 'white',
-                  padding: '5px 10px',
-                  marginTop: '15px',
-                  marginRight: '20px',
-                  borderRadius: '10px',
-                }}
-              >
-                {tag}
+            {tags.length > 0 ? (
+              tags.map((tag, index) => (
+                <div
+                  key={index}
+                  style={{
+                    backgroundColor: '#80D1D5',
+                    color: 'white',
+                    padding: '5px 10px',
+                    marginTop: '15px',
+                    marginRight: '20px',
+                    borderRadius: '10px',
+                  }}
+                >
+                  {tag}
+                </div>
+              ))
+            ) : (
+              <div style={{ fontStyle: 'italic', color: '#858585', marginTop: '15px' }}>
+                Tags haven't been set
               </div>
-            ))}
+            )}
             {userId === user._id && (
               <Link to={`/EditProfile/${userId}`}>
-                <img src={EditProfileIcon} alt="edit profile" style={{ width: '30px', height: '30px', marginLeft: '200px', marginTop: '15px' }} />
+                <img
+                  src={EditProfileIcon}
+                  alt="edit profile"
+                  style={{ width: '30px', height: '30px', marginLeft: '20px', marginTop: '15px' }}
+                />
               </Link>
             )}
           </div>
