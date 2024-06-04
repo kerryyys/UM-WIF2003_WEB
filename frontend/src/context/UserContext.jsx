b08cea601672ac94a16f67ece3ce591718542a1a
fyiernzy <fyiernzy@gmail.com>
2024-06-04 22:13:16 +0800

    Fix UserContext, everytime login a new page will check cookies first

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { verifyUserToken } from "../api/authApi";
 
 const UserContext = createContext();
 
@@ -7,6 +8,18 @@ export const useUserContext = () => useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const checkUserToken = async () => {
+      const result = await verifyUserToken();
+      console.log("In User Context, the result status is=", result.status);
+      if (result.status) {
+        setUser(result.user);
+      }
+    };
+
+    checkUserToken();
+  }, []);
 
   const updateUser = (newUserData) => {
     // update user here
