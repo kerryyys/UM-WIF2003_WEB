378c48363c049191ff8f275573abca00deb66989
user <22004795@siswa.um.edu.my>
2024-06-05 17:01:31 +0800

    fix error

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -19,7 +19,6 @@ import {
 } from "../controllers/projectsController.js";
 import mongoose, { mongo } from "mongoose";
 import { downloadFile } from "../middlewares/downloadMiddleware.js";
-import { getPostedProjects } from "../../frontend/src/api/projectApi.jsx";
 const router = e.Router();
 // Configure storage for multer
 const storage = multer.diskStorage({
@@ -72,9 +71,6 @@ router.get("/completed-project/:userId", getCompletedProjects);
 router.post("/upload-works", upload.array("files"), uploadCompletedWorks);
 // GET /projects/:projectId - Retrieves project details of projectId
 router.get("/:projectId", getProjectDetails);
-
-//GET /posted-project - Retrieves all posted project of current user
-router.get("/posted-project/:userId",getPostedProjects);
 
 // router.post("/get-applicants", async (req, res) => {
 //   try {
